02bd1018b122b06883bc646946b0be7fcb7792e3
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2021-02-14 00:25:01 +0530

    Finished majority of about , have to add curtain effect and soem para styling

--- about_us/aboutus.js
+++ about_us/aboutus.js
@@ -1,7 +1,7 @@
 fetch("../navbar/navbar.html")
   .then((response) => {
     if (response.status != 200) {
-      throw "Navbar - File path not correct or JS malfuction.";
+      throw "Navbar - File path not correct or JS malfuction. in about us";
     }
     return response.text();
   })
@@ -25,14 +25,39 @@ const addContent = function () {
 
     Description_h1.innerHTML = d_name;
     Description_p.innerHTML = d_desc;
+    main.innerHTML = "Get to know us :)"
 }; 
+
+const removeContent = function () {
+
+    Description_h1.innerHTML = "";
+    Description_p.innerHTML = "";
+    for (let i = 0; i < img_d.length; i++) {
+      console.log(this == img_d[i]);
+    }
+
+}
 
 const img_d= document.getElementsByClassName("author");
 const Description_h1 = document.querySelector(".authorcontent h1");
 const Description_p = document.querySelector(".authorcontent p");
+const main = document.querySelector("h1");
 
 for (let i = 0; i < img_d.length; i++) {
-    img_d[i].addEventListener("mouseover", addContent)
+    img_d[i].addEventListener("mouseover", addContent);
+    img_d[i].addEventListener("mouseleave", removeContent);
 }
 
 
+// const av = document.querySelector("a");
+// const sec = document.querySelector("section");
+
+// av.addEventListener("onclick", (e) => {
+//     e.preventDefault();
+//     sec.classList.toggle("muestra")
+// })
+
+// // $("a").on("click", function (e) {
+// //   e.preventDefault();
+// //   $("section").toggleClass("muestra");
+// // });
