86cd6ad4527e8ae676344ac1d7d39fa6a50acf43
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2021-02-15 22:31:58 +0530

    Added some login page additions

---
+++ login/login.js
@@ -0,0 +1,8 @@
+const username = document.querySelector("#user");
+const password = document.querySelector("#pass");
+
+
+
+username.addEventListener("onchange", checkSubmit);
+password.addEventListener("onchange", checkSubmit);
+
