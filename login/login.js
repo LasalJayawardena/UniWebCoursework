e20c5e1d629bcf33b9e8873f4fdbadf4a03a0b57
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2021-02-15 23:29:29 +0530

    Added hash functionality

--- login/login.js
+++ login/login.js
@@ -1,7 +1,26 @@
 const username = document.querySelector("#user");
 const password = document.querySelector("#pass");
-const btn = document.querySelectorAll("#btn");
+const btn = document.querySelector("#btn");
 
+// passwords are lasal, chamx, crazywheels
+const dummy_users = {
+  "lasal@gmail.com":"BwlixQ0wSskDbDujbwBSq2ndMSyZZEbvk4YBwvIJUk8=",
+  "chamix@hotmail.com":"mifS/bLRsUkqUL52ovbIFDsSdn4y4a9DZB+IuIXhWQY=",
+  "nitiwt@hotwheels.org":"I4Y2hn2iW05YYcDEile4Cwf/97/cC8H7GfoNprM2s7c="
+}; 
+
+// Hash the password to check
+async function hash(target){
+   var buffer = await crypto.subtle.digest("SHA-256", new TextEncoder().encode(target));
+   var chars = Array.prototype.map.call(new Uint8Array(buffer), ch => String.fromCharCode(ch)).join('');
+   return btoa(chars);
+};
+
+// const test = async () =>{
+//     let j = await hash("crazywheels");
+//     console.log(j);
+// }
+// test()
 
 const isEmail = (email) => {
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -15,23 +34,49 @@ const valid_username = (name) => {
         valid = false;
     }
     if(! isEmail(name)){
-        valid= false;
+        valid = false;
     }
     return valid;
 }
 
 const valid_password = (pass) => {
-    return pass.length > 5 && pass.length < 20;
+    return pass.length > 4 && pass.length < 20;
+}
+
+const valid_cred = (uname,  pass)  =>{
+    let v_uname = valid_username(uname);
+    let v_pass = valid_password(pass);
+
+    return (v_uname && v_pass);
 }
 
 const checkSubmit = function(){
     let uname = username.value;
     let pass = password.value;
-    let v_uname = valid_username(uname);
-    let v_pass = valid_password(pass);
-    console.log(v_uname, v_pass);
+
+    if(valid_cred(uname, pass)){
+        btn.disabled = false;
+    }
 } 
+
+const handleSubmit = async function(e){
+    e.preventDefault();
+    let uname = username.value;
+    let pass = password.value;
+
+    if (!valid_cred(uname, pass)) {
+        btn.disabled = true;
+        return
+    }
+
+    let hashed =  await hash(pass);
+    if((uname in dummy_users) && (dummy_users[uname] == hashed)){
+        console.log("noice")
+    }
+    console.log("failed");
+
+}
 
 username.addEventListener("keyup", checkSubmit);
 password.addEventListener("keyup", checkSubmit);
-
+btn.addEventListener("click", handleSubmit);
