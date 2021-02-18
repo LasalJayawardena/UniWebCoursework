2448b91723b02882972f2c3f9c207371f5870449
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2021-02-19 00:09:55 +0530

    Addedmodal forpopu

---
+++ user_signup/user_signup.js
@@ -0,0 +1,26 @@
+const openEl = document.querySelector("[data-open]");
+const closeEl = document.querySelector("[data-close]");
+const isVisible = "is-visible";
+const modal_win = document.getElementById("modal");
+
+openEl.addEventListener("click", function () {
+    modal_win.classList.add(isVisible);
+});
+
+
+closeEl.addEventListener("click", function () {
+  modal_win.classList.remove(isVisible);
+});
+
+
+document.addEventListener("click", (e) => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    modal_win.classList.remove(isVisible);
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    modal_win.classList.remove(isVisible);
+  }
+});
