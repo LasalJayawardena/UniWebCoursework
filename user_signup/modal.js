af0bbf770bca9429683a7e1850cdea339954d3cf
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2021-03-21 19:09:15 +0530

    Added editopage

--- user_signup/modal.js
+++ user_signup/modal.js
@@ -5,25 +5,25 @@ const modal_win = document.getElementById("modal");
 
 // Closes the modal
 const closeModal = () => {
-  modal_win.classList.remove(isVisible);
+    modal_win.classList.remove(isVisible);
 };
 
 // Checks when the user clicks on the webpage,
 // Whether the the modal is visible and whether he clicked out of the modal container
 // If so then close modal.
 document.addEventListener("click", (e) => {
-  if (e.target == document.querySelector(".modal.is-visible")) {
-    closeModal();
-  }
+    if (e.target == document.querySelector(".modal.is-visible")) {
+        closeModal();
+    }
 });
 
 // Checks when the user presses a key,
 // Whether the the modal is visible and whether he type the esc key
 // If so then close modal.
 document.addEventListener("keyup", (e) => {
-  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
-    closeModal();
-  }
+    if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+        closeModal();
+    }
 });
 
 // Functionality to implement Redirecting
@@ -33,12 +33,11 @@ const redirect = () => {
     redirectMsg.innerHTML = `Dear ${Mname.value}, Thank you for registering with Euphoria, You will be directed to our Gallery page.`;
     let time = 6;
     let t = setInterval(() => {
-      time--;  
-      timepan.innerHTML = `${time}s`;  
-      if (time == 0) {
-        window.location.replace("../main.html");
-        clearInterval(t);
-      }
+        time--;
+        timepan.innerHTML = `${time}s`;
+        if (time == 0) {
+            window.location.replace("../main.html");
+            clearInterval(t);
+        }
     }, 1000);
 }
-
