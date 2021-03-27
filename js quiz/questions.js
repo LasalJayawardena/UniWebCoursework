e490dab8a3ed2ab6a29cdb10649b179e56154b1d
Nithila Ariyapperuma <72573192+nitwit45@users.noreply.github.com>
2021-03-27 14:19:43 +0530

    Add files via upload

---
+++ js quiz/questions.js
@@ -0,0 +1,80 @@
+let MCQS = [{
+    question: "How is covid-19 passed on?",
+    answer1: "Through droplets that come from nose and mouth",
+    answer2: "By drinking unclean water",
+    answer3: "All of above",
+    answer4: "Can be sexually transmitted",
+    answer: 0
+},
+            {
+            question: "What are the common symptoms of covid-19?",
+    answer1: "A new and continuous cough",
+    answer2: "Fever",
+    answer3: "Tiredness",
+    answer4: "All of the above",
+    answer: 3
+},
+            {
+            question: "Can you always tell if someone has covid-19?",
+    answer1: "No, not everyone with covid-19 has symptoms",
+    answer2: "Yes, it will be obvious,a person with covid-19 coughs a lot",
+    answer3: "Yes, you can tell just by where a person comes from,their race and ethnicity",
+    answer4: "Yes, they have Blue Patches on skin",
+    answer: 0
+},
+            {
+            question: "Can washing your hands protect you from covid-19?",
+    answer1: "Yes, but only if you a strong bleach",
+    answer2: "Yes, normal soap and water or hand sanitizer is enough",
+    answer3: "No, washing your hands doesn't stop Covid-19",
+    answer4: "No, only God can protect you from Covid-19",
+    answer: 1
+},
+            {
+            question: "Which of the following people is covid-19 more dangerous for?",
+    answer1: "Children",
+    answer2: "Older people and People with certain underlying health conditions",
+    answer3: "European People",
+    answer4: "Asian People",
+    answer: 1
+},
+    {
+        question: "Each of the following statements is True, Except :",
+        answer1: "Viruses come in different types",
+        answer2: "Viruses infect living cells",
+        answer3: "Viruses can replicate without hosts",
+        answer4: "Viruses can cause illnesses",
+        answer: 2
+    },
+    {
+        question: "When should fabric face masks be worn?",
+        answer1: "On public transport",
+        answer2: "In confined or crowded spaces",
+        answer3: "In small shops",
+        answer4: "All of the above",
+        answer: 3
+    }, {
+        question: "Can covid-19 be cured?",
+        answer1: "Yes - hot drinks can cure covid-19",
+        answer2: "No - covid-19 is a death sentence",
+        answer3: "No - but most people get better by themselves",
+        answer4: "Yes - Sunbathing can cure covid-19",
+        answer: 2
+    },
+    {
+        question: "Which of the following is an example of physical distancing?",
+        answer1: "You stop going to crowded places and visiting other people houses",
+        answer2: "You stop talking to the people you live with",
+        answer3: "Older people and People with certain underlying health conditions",
+        answer4: "You stop sleeping with your partner",
+        answer: 2
+    },
+    {
+        question: "What does the 19 in covid-19 stand for?",
+        answer1: "It is the year the virus was first encountered",
+        answer2: "It refers to the 19 molecules that make up the virus",
+        answer3: "It is the 19th coronavirus identifies since the WHO began naming them",
+        answer4: "It says how many years will covid-19 lasts",
+        answer: 0
+    }
+];
